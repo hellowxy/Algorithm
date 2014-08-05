9cc928563ade20661df43f194bca37240c2b45b4
Wang Xiaoying <wangxiaoying_op@163.com>
2014-08-05 15:48:11 +0800

    创建项目

---
+++ Algorithm/Sort.cs
@@ -0,0 +1,45 @@
+﻿//****************************************************************************************************
+//ClassName:       Sort
+//Author:          Xiaoying Wang
+//Guid:		       7cdbf5ef-dee9-4495-a550-84f351bd52f2
+//DateTime:        2014/8/5 14:14:18
+//Email Address:   wangxiaoying@gedu.org
+//FileName:        Sort
+//CLR Version:     4.0.30319.18444
+//Machine Name:    WXY-PC
+//Namespace:       Algorithm
+//Function:                
+//Description:    Implementation of popular sort algorithms, include insertion sort, quicksort, merge sort and heapsort
+//****************************************************************************************************
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    public static class Sort
+    {
+        public static T[] InsertionSort<T>(T[] arr, IComparer<T> comparer)
+        {
+            return null;
+        }
+
+        public static T[] MergeSort<T>(T[] arr, IComparer<T> comparer)
+        {
+            return null;
+        }
+
+        public static T[] QuickSort<T>(T[] arr, IComparer<T> comparer)
+        {
+            return null;
+        }
+
+        public static T[] HeapSort<T>(T[] arr, IComparer<T> comparer)
+        {
+            return null;
+        }
+    }
+}
