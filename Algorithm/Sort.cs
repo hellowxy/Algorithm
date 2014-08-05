92695cbd9b5ca2b1d3f006545fb19d755204c54a
Wang Xiaoying <wangxiaoying_op@163.com>
2014-08-05 17:13:26 +0800

    完成插入排序

--- Algorithm/Sort.cs
+++ Algorithm/Sort.cs
@@ -22,24 +22,59 @@ namespace Algorithm
 {
     public static class Sort
     {
-        public static T[] InsertionSort<T>(T[] arr, IComparer<T> comparer)
+        private static void Guard<T>(T[] arr, ref IComparer<T> comparer)
         {
+            if (arr == null || arr.Length == 0)
+            {
+                throw new ArgumentException();
+            }
+            var tType = typeof(T);
+            if (comparer == null && (typeof(IComparable<T>).IsAssignableFrom(tType)) ||
+                typeof(IComparable).IsAssignableFrom(tType))
+            {
+                comparer = Comparer<T>.Default;
+            }
+        }
+        #region insertion sort
+        public static T[] InsertionSort<T>(T[] arr, IComparer<T> comparer = null)
+        {
+            Guard(arr, ref comparer);
+            var ret = new T[arr.Length];
+            ret[0] = arr[0];
+            var count = 1;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                for (int j = count - 1; j >= -1; j--)
+                {
+                    if (j == -1 || comparer.Compare(arr[i], ret[j]) >= 0)
+                    {
+                        ret[j+1] = arr[i];
+                        count++;
+                        break;
+                    }
+                    ret[j + 1] = ret[j];
+                }
+            }
+
+            return ret;
+        }
+        #endregion
+
+        public static T[] MergeSort<T>(T[] arr, IComparer<T> comparer = null)
+        {
+            Guard(arr, ref comparer);
             return null;
         }
 
-        public static T[] MergeSort<T>(T[] arr, IComparer<T> comparer)
+        public static T[] QuickSort<T>(T[] arr, IComparer<T> comparer = null)
         {
+            Guard(arr, ref comparer);
             return null;
         }
 
-        public static T[] QuickSort<T>(T[] arr, IComparer<T> comparer)
+        public static T[] HeapSort<T>(T[] arr, IComparer<T> comparer = null)
         {
-            return null;
-        }
-
-        public static T[] HeapSort<T>(T[] arr, IComparer<T> comparer)
-        {
-            //
+            Guard(arr, ref comparer);
             return null;
         }
     }
